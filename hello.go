455893b2f8782e5d5f6daaaceeecc8602bf33f75
Irene <irene@aurametrix.com>
2013-09-03 08:14:44 -0700

    added more interaction

--- hello.go
+++ hello.go
@@ -3,10 +3,28 @@ package main
 import (
     "fmt"
     "time"
+    "math/rand"
     )
 
-func main() {
-    fmt.Printf("hello, world\n")
-    fmt.Println("The time is", time.Now())
+var x, y, z = true, "no!", "what's your name?"
 
+func add(x int, y int) int {
+    return x + y
+}
+
+func main() {
+    const You = "世界"
+    var i int
+    var name string
+    
+    fmt.Printf("hello, world\n")
+    fmt.Println("Are you", You, x, y, z)
+    fmt.Scan(&name );
+    fmt.Println("Hello", name)
+    
+    fmt.Println("The time is", time.Now())
+    fmt.Println("My favorite number is", add(4,rand.Intn(10)))
+    fmt.Println("yours?")
+    fmt.Scan(&i)
+    fmt.Println("Your favorite number is", i, "!")
 }
