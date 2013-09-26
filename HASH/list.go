00e2a91e376a17705dcd13973f9a5462c0f4b45a
Irene <irene@aurametrix.com>
2013-09-26 11:00:46 -0700

    implementing a simple list

---
+++ HASH/list.go
@@ -0,0 +1,24 @@
+//Package list implements a doubly linked list.
+
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	// Create a new list and populate it with numbers
+	l := list.New()
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	l.InsertBefore(3, e4)
+	l.InsertAfter(2, e1)
+
+	// Iterate through list and and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+
+}
+
