340b5e7a4655d9b456c8476d67151e7613a3b9af
Irene <irene@aurametrix.com>
2013-10-19 15:59:39 -0700

    completed the blank img code

--- IMG/blank.go
+++ IMG/blank.go
@@ -1,3 +1,13 @@
+package main
+
+import (
+        "image"
+        "image/png"
+        "log"
+        "os"
+)
+
+func main() {
 width, height := 128, 128
 m := image.NewRGBA(image.Rect(0, 0, width, height))
 out_filename := "blank.png"
@@ -8,3 +18,4 @@ if err != nil {
 defer out_file.Close()
 log.Print("Saving image to: ", out_filename)
 png.Encode(out_file, m)
+}
