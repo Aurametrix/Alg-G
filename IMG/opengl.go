56a3c1b4ecaa32da006907dd85c5873001f8db04
Irene <irene@aurametrix.com>
2015-02-25 16:07:51 -0800

    Open GL example to make a Gopher cube

---
+++ IMG/opengl.go
@@ -0,0 +1,31 @@
+func main() {
+	glfw.SetErrorCallback(errorCallback)
+
+	if !glfw.Init() {
+		panic("Can't init glfw!")
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(Width, Height, Title, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	glfw.SwapInterval(1)
+
+	gl.Init()
+
+	if err := initScene(); err != nil {
+		fmt.Fprintf(os.Stderr, "init: %s\n", err)
+		return
+	}
+	defer destroyScene()
+
+	for !window.ShouldClose() {
+		drawScene()
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
