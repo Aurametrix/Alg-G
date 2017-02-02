891b7cbdc66c7505ede28dc2433889bdba24895d
Aurametrix <irene@aurametrix.com>
2017-02-02 11:21:50 -0800

    cross platform actor support between Go and C#
    https://github.com/AsynkronIT/protoactor-go
    http://proto.actor/
    https://news.ycombinator.com/item?id=13550288

---
+++ CROSS-PLATFORM/hello-go-c.go
@@ -0,0 +1,16 @@
+type Hello struct{ Who string }
+type HelloActor struct{}
+
+func (state *HelloActor) Receive(context actor.Context) {
+    switch msg := context.Message().(type) {
+    case Hello:
+        fmt.Printf("Hello %v\n", msg.Who)
+    }
+}
+
+func main() {
+    props := actor.FromInstance(&HelloActor{})
+    pid := actor.Spawn(props)
+    pid.Tell(Hello{Who: "Roger"})
+    console.ReadLine()
+}
