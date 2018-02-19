e7037dad39e4485a30b27d8ac3dff9e44da3dc5d
Aurametrix <irene@aurametrix.com>
2018-02-19 09:57:11 -0800

    reasons to dislike golang
    https://www.teamten.com/lawrence/writings/why-i-dont-like-go.html

---
+++ Misc/dontlike.go
@@ -0,0 +1,36 @@
+type user struct {
+    name string
+}
+
+func main() {
+    var user *user
+
+    user = &user{}    // Compile error
+}
+
+db.Exec("DELTE FROM item WHERE id = 2")
+
+user, err := getUserById(userId)
+
+var serializeType = showArchived ? model.SerializeAll : model.SerializeNonArchivedOnly
+
+// you’re forced to this imperative verbosity:
+
+var serializeType model.SerializeType
+if showArchived {
+    serializeType = model.SerializeAll
+} else {
+    serializeType = model.SerializeNonArchivedOnly
+}
+
+sort.Sort(sort.Reverse(UsersByLastSignedInAt(users)))
+
+stack = append(stack, object)
+
+
+object = stack[len(stack) - 1]
+stack = stack[:len(stack) - 1]
+
+users = append(users, newUser)
+
+append(users, newUser)
