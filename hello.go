4f003ab35e093b760789d6c31c9fb2cee09e4563
Aurametrix <irene@aurametrix.com>
2018-04-30 06:13:59 -0700

    with gophers
    https://news.ycombinator.com/item?id=16957307
    https://medium.com/@caspervonb/hello-go-3e207da92da0

--- hello.go
+++ hello.go
@@ -27,4 +27,28 @@ func main() {
     fmt.Println("yours?")
     fmt.Scan(&i)
     fmt.Println("Your favorite number is", i, "!")
+
+
+  t := "Hello world!"
+  c := make(chan string, 32)
+  for i := 0; i < 32; i++ {
+    go gopher(t, c)
+  }
+  for s := range c {
+    fmt.Println(s)
+    if s == t {
+      break
+    }
+  }
 }
+
+func gopher(t string, c chan string) {
+  s := []rune(t)
+  for {
+    rand.Shuffle(len(s), func(i, j int) {
+      s[i], s[j] = s[j], s[i]
+    })
+    c <- string(s)
+  }
+}
+
