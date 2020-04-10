be4c3f72902ef1e13619e60705510764e04aadd9
Aurametrix <irene@aurametrix.com>
2020-04-09 20:00:48 -0400

    safer and easier to use implementation
    https://blog.tempus-ex.com/generics-in-go-how-they-work-and-how-to-play-with-them/
    https://news.ycombinator.com/item?id=22818772

--- Misc/FIFO.go
+++ Misc/FIFO.go
@@ -1,13 +1,13 @@
-type Stack []interface{}
+type Stack(type T) []T
 
-func (s Stack) Peek() interface{} {
+func (s Stack(T)) Peek() T {
 	return s[len(s)-1]
 }
 
-func (s *Stack) Pop() {
+func (s *Stack(T)) Pop() {
 	*s = (*s)[:len(*s)-1]
 }
 
-func (s *Stack) Push(value interface{}) {
+func (s *Stack(T)) Push(value T) {
 	*s = append(*s, value)
 }
