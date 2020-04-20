af989a971ad77adb4655a119b3948807e61a7c4e
Aurametrix <irene@aurametrix.com>
2020-04-19 21:43:44 -0400

    routes incoming livestream data from websockets to an external RTMP endpoint.
    https://github.com/chuabingquan/pxy
    https://news.ycombinator.com/item?id=22919845

---
+++ Misc/cmd-pxy-main.go
@@ -0,0 +1,9 @@
+// https://github.com/chuabingquan/pxy
+// cmd/pxy/main.go
+// Access http://localhost:8080
+
+const (
+	readBufferSize  = 1024
+	writeBufferSize = 1024
+	publishURL      = "rtmp://global-live.mux.com:5222/app" // This one here.
+)
